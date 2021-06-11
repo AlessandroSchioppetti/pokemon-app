bd1a4ab1c1c63b142d3639ec4f802974e2657248
Alessandro Schioppetti <a.schioppetti@codermine.com>
2021-06-11 20:00:48 +0200

    feat: added splash controller, + refactor PokemonService

---
+++ pokemon-app/Controllers/SplashViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SplashViewController.swift
+//  pokemon-app
+//
+//  Created by Alessandro Schioppetti on 11/06/2021.
+//
+
+import Foundation
---
+++ pokemon-app/Extension/UIView.swift
@@ -0,0 +1,8 @@
+//
+//  UIView.swift
+//  pokemon-app
+//
+//  Created by Alessandro Schioppetti on 11/06/2021.
+//
+
+import Foundation
